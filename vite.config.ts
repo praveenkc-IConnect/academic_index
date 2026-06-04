866ad7b2036c890d0bb18f719e0f193096e4c74f
praveenkc-IConnect <praveenkc@tkmit.ac.in>
2026-06-04 22:26:18 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './', // <-- Change this line to relative dot-slash
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
