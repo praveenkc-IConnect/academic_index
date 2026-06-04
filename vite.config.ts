e8255fb1816349660cee568c2e4aa6494d2f9e5b
praveenkc-IConnect <praveenkc@tkmit.ac.in>
2026-06-04 22:03:57 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,11 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/academic_index/', // Moved to the strict top-level object
+  base: '/academic_index/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
     },
-  },
-  server: {
-    hmr: process.env.DISABLE_HMR !== 'true',
-    watch: process.env.DISABLE_HMR === 'true' ? null : {},
   },
 });
