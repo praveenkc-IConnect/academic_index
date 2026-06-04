e17549a59d0573175d0ec141b23f12a6a950de37
praveenkc-IConnect <praveenkc@tkmit.ac.in>
2026-06-04 23:04:13 +0530

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App'; // <-- Removed the curly braces
+import { AcademicModule } from './AcademicModule';
 import './index.css';
 
 const container = document.getElementById('root');
@@ -11,6 +11,6 @@ if (!container) {
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <App />
+    <AcademicModule />
   </React.StrictMode>
 );
